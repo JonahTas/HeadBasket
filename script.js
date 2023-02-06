25b29e778fac7dd8a07eed5a1e83acb8798efe39
Jonah Tas <90318569+JonahTas@users.noreply.github.com>
2023-02-06 09:46:44 +0000

    Setup

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("hooray")
+var gamestate = 2; //tijdelijk voor testen game
+
+
+
+
+
+//---------------------------------------------------------------//
+function preload() {
+  GAMEBACKGROUND = loadImage('Images/BasketballField.png');
+}
+//---------------------------------------------------------------//
+function setup() {
+  createCanvas(1000, 500);
+}
+//---------------------------------------------------------------//
+function draw() {
+
+  if (gamestate == 0) {
+    Startscherm();
+  }
+
+  if (gamestate == 1) {
+    Playerpicker();
+
+  }
+
+  if (gamestate == 2) {
+    Maingame();
+  }
+
+  if (gamestate == 3) {
+    Resultscreen();
+  }
+}
+//---------------------------------------------------------------//
+function Startscherm() {
+  
+}
+//---------------------------------------------------------------//
+function Playerpicker() {
+  
+}
+//---------------------------------------------------------------//
+function Maingame() {
+  background(100);
+  //image(GAMEBACKGROUND,0,0,1000,500);
+}
+//---------------------------------------------------------------//
+function Reultscreen() {
+  
+}
+//---------------------------------------------------------------//
