d7ffd9ddd044bec861de7e501d0a1a134ecbcf27
JonahTas <90318569+JonahTas@users.noreply.github.com>
2023-04-13 13:11:15 +0000

    1e button class aangepast

--- buttons.js
+++ buttons.js
@@ -1,20 +1,14 @@
 class Button {
-  constructor(x, y, w, h, text, size, action) {
+  constructor(x, y, w, h, image, action) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
-    this.text = text;
-    this.size = size;
+    this.image = image;
     this.action = action;
   }
   drawButton() {
-    fill(255, 0, 0,255)
-    rect(this.x, this.y, this.w, this.h);
-    fill(255,255,255,255)
-    textAlign(CENTER, CENTER);
-    textSize(this.size);
-    text(this.text, this.x + this.w * 0.5, this.y + this.h * 0.5);
+    image(this.image, this.x, this.y, this.w, this.h);
   }
   clicked() {
     if (mouseX > this.x && mouseX < (this.x + this.w) && mouseY > this.y && mouseY < (this.y + this.h)) {
