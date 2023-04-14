adcd0738b858788d96ac031538b7873b5a18c10c
JonahTas <90318569+JonahTas@users.noreply.github.com>
2023-04-14 08:52:03 +0000

    Kleine bug aangepast met buttons die worden geclicked als je nog in het startscherm zit

--- mouseclicked.js
+++ mouseclicked.js
@@ -5,20 +5,20 @@ function mouseClicked() {
     controls.clicked();
 
   }
-  if (gamestate == 0.5) {
+  else if (gamestate == 0.5) {
     back.clicked();
   }
-  if (gamestate == 4) {
+  else if (gamestate == 4) {
     back.clicked();
   }
-  if (gamestate == 2) {
+  else if (gamestate == 2) {
     pause.clicked();
   }
-  if (gamestate == 6) {
+  else if (gamestate == 6) {
     Continue.clicked();
     Menu.clicked();
   }
-  if (gamestate == 1) {
+  else if (gamestate == 1) {
     skin1.clicked();
     skin2.clicked();
     skin3.clicked();
@@ -28,5 +28,4 @@ function mouseClicked() {
   }
   storeItem('valSound', valSound);
   storeItem('valMusic', valMusic);
-  console.log(mouseX,mouseY);
 }
