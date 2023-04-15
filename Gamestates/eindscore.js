051cba794387cedf9ac86e03f2b49a88f05e134f
NienkeLemain1 <9851313-NienkeLemain1@users.noreply.replit.com>
2023-04-15 18:51:13 +0000

    Eindscore klopte eerst niet want als speler 1 won stond er dat speler 2 won maar nu klopt het yay

--- Gamestates/eindscore.js
+++ Gamestates/eindscore.js
@@ -4,11 +4,11 @@ image(EINDSCOREBACKGROUND, 0, 0, WIDTH, HEIGHT);
 Menu.drawButton();
 
   
-  if (scoreP1 > scoreP2) {
+  if (scoreP1 < scoreP2) {
     text("Player 1 won!",500,90); //wnr speler 1 wint
   }
   
-  if (scoreP1 < scoreP2) {
+  if (scoreP1 > scoreP2) {
     text("Player 2 won",500,90); // wnr speler 2 wint
   } 
   if (scoreP1 == scoreP2) {
